f7a7424d0893bf4cc45e5195d42885cc02f9c84b
Yannis Cosmadopoulos <yannisc@gmail.com>
2011-09-23 09:04:32 +0300

    (shallow) fix to broken build

--- src/test/java/domain/ExperimentalTest.java
+++ src/test/java/domain/ExperimentalTest.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 import javax.cache.Cache;
 import javax.cache.CacheLoader;
 import javax.cache.CacheManager;
-import javax.cache.CacheManagerFactory;
+import javax.cache.Caching;
 import javax.cache.event.CacheEntryListener;
 import javax.cache.event.NotificationScope;
 
+import java.sql.Connection;
 import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
@@ -17,8 +18,27 @@ import static org.junit.Assert.assertNull;
 public class ExperimentalTest {
     private static final String CACHE_NAME = "myCache";
 
+    public enum TxIsolationLevels {
+        TX_NONE(java.sql.Connection.TRANSACTION_NONE),
+        TX_READ_UNCOMMITTED(java.sql.Connection.TRANSACTION_READ_UNCOMMITTED),
+        TX_READ_COMMITTED(java.sql.Connection.TRANSACTION_READ_COMMITTED),
+        TX_READ_REPEATABLE(java.sql.Connection.TRANSACTION_REPEATABLE_READ),
+        TX_READ_SERIALIZABLE(java.sql.Connection.TRANSACTION_SERIALIZABLE);
+
+        private final int value;
+        private TxIsolationLevels(int value) {
+            this.value = value;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+    }
+
+
     private void init(String cacheName, int[] keys) {
-        CacheManager cacheManager = CacheManagerFactory.getCacheManager();
+        CacheManager cacheManager = Caching.getCacheManager();
         Cache<Integer, String> myCache = cacheManager.<Integer, String>createCacheBuilder(cacheName).build();
         for (int key : keys) {
             myCache.put(key, "value" + key);
@@ -28,7 +48,7 @@ public class ExperimentalTest {
     public void doIt() {
         int[] keys = {1, 2, 3, 4, 5};
         init(CACHE_NAME, keys);
-        Cache<Integer, String> myCache = CacheManagerFactory.getCacheManager().getCache(CACHE_NAME);
+        Cache<Integer, String> myCache = Caching.getCacheManager().getCache(CACHE_NAME);
         for (int key : keys) {
             assert myCache.containsKey(key);
         }
@@ -38,7 +58,7 @@ public class ExperimentalTest {
 
     public void foo() {
         String cacheName = "sampleCache";
-        CacheManager cacheManager = CacheManagerFactory.getCacheManager();
+        CacheManager cacheManager = Caching.getCacheManager();
         Cache<Integer, Date> cache = cacheManager.getCache(cacheName);
         CacheLoader cl = null;
         CacheEntryListener listener1 = null;
