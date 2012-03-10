2c9fc8eea71208ab78ef86b62cbaa2e3b918d1c1
Yannis Cosmadopoulos <yannisc@gmail.com>
2012-03-10 10:20:17 -0800

    no more 107 here

---
+++ src/test/java/experiment/FooTest.java
@@ -0,0 +1,10 @@
+package experiment;
+
+/**
+ * Copyright (c) 2012 Terracotta, Inc.
+ * Copyright (c) 2012 Oracle and/or its affiliates.
+ * <p/>
+ * All rights reserved. Use is subject to license terms.
+ */
+public class FooTest {
+}
